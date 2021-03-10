f61690725342972c415afaf806aeec2b948879b7
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-03-10 13:09:38 -0800

    working on human player file

--- humanPlayer.js
+++ humanPlayer.js
@@ -1,14 +1,25 @@
+const readline = require("readline")
+let battleShipGame = require("./game.js")
+
 class HumanPlayer {
   constructor() {
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
+    this.rl = readline.createInterface(process.stdin, process.stdout);
+
+
   }
 
-  getMove() {
+  getMove(processMove) {
     // TODO: Ask the user for their move and process the answer using the rl
     // interface.Invoke the callback function (processMove), passing in
     // the given answer in the form of an array representing [row, col]
+    readline.Interface.processMove(answer)
+    this.rl.question('what?', answer => {
+      // do something with the answer
+      // this.rl.close()...to close the readline interface
+    })
   }
 
   processGameOver(isWon) {
