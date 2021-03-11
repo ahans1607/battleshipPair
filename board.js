7149be53d7f526e70a6d1c90b6c85fdf97a816cf
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-03-10 16:21:29 -0800

    stuck on display method

--- board.js
+++ board.js
@@ -1,5 +1,5 @@
 class Board {
-  constructor() {
+  constructor(numRos, numCols, numShips) {
     // TODO: Set up constructor that sets the numRos, numCols, and numShips.
     this.numRos = numRos;
     this.numCols = numCols;
@@ -12,12 +12,47 @@ class Board {
   populateGrid() {
     // TODO: Using the instance variables numRows, numCols, and numShips, return
     // a 2D array representing the state of the board.
+    let gridArray = []
+    for (let i = 0; i < this.numRos; i++) {
+      const row = []
+      for (let j = 0; j < this.numCols; j++) {
+        row.push('~')
+      }
+      gridArray.push(row)
+    }
+    let count = this.numShips
+    while (count > 0) {
+      // console.log(count)
+      const randomRow = Math.floor(Math.random() * this.numRos)
+      const randomCol = Math.floor(Math.random() * this.numCols)
+      if (gridArray[randomRow][randomCol] === '~') {
+        gridArray[randomRow][randomCol] = 's'
+        count--
+      }
+    }
+    return gridArray
   }
 
   display() {
     // TODO: Print the game board with marks on any spaces that have been fired
     // upon. Be sure not to display the unhit ships to the user! Hint: you might
     // be able to use console.table()
+    for (let i = 0; i < this.grid.length; i++) {
+      let firstArray = this.grid[i]
+      // console.log("i index",firstArray)
+      for (let j = 1; j < this.grid.length; j++) {
+        let secondArray = this.grid[j]
+        console.log("i index", firstArray[i])
+        console.log("index j", secondArray[i])
+        if (firstArray[i] === 's') {
+          firstArray[i] = '~'
+        }
+        if (secondArray[i] === 's') {
+          secondArray[i] = '~'
+        }
+       }
+    }
+    console.table(this.grid)
   }
 
   count() {
@@ -39,5 +74,9 @@ class Board {
     // space or a damaged ship.
   }
 }
+
+const check = new Board(5, 5, 3)
+console.table(check.grid)
+check.display()
 
 module.exports = Board;
